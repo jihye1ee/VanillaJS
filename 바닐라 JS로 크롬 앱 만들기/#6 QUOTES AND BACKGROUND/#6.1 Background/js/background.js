b778cacc41a8aed72bfdb12750a6634e45b1462b
이지혜 <89819830+jihye1ee@users.noreply.github.com>
2023-07-07 09:37:04 +0900

    오류 수정

--- 바닐라 JS로 크롬 앱 만들기/#6 QUOTES AND BACKGROUND/#6.1 Background/js/background.js
+++ 바닐라 JS로 크롬 앱 만들기/#6 QUOTES AND BACKGROUND/#6.1 Background/js/background.js
@@ -4,7 +4,3 @@ const bgImage = document.createElement("img"); // element 생성
 
 bgImage.src = `img/${chosenImage}`;
 document.body.appendChild(bgImage); // body에 bgImage(html element)을 추가
-/*
-  - prependChild -> 가장 앞에 추가
-  - appendChild -> 가장 뒤에 추가
-*/
